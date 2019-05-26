ea16f333cda7b141edf808eb88b01eda5bdfe5c7
juansalvatore <juansalvatore.ar@gmail.com>
2019-05-26 12:47:39 -0300

    Agregar test para cola

--- estructuras_aux/Cola.py
+++ estructuras_aux/Cola.py
@@ -3,17 +3,29 @@ class Cola:
         self.items = []
 
     def encolar(self, dato):
+        """
+            Agrega el dato al final de la cola.
+        """
         self.items = [dato] + self.items
 
     def desencolar(self):
+        """
+            Desencola el primer elemento que fue encolado y retorna el dato desencolado.
+        """
         if self.items == []:
             raise Exception('Cola vacía.')
         return self.items.pop()
 
     def ver_frente(self):
+        """
+            Retorna el frente de la cola
+        """
         if self.items == []:
-            raise Exception('Cola vacía.')
-        return self.items[0]
+            return None
+        return self.items[len(self.items) - 1]
 
     def esta_vacia(self):
+        """
+            Retorna si la cola esta o no vacía.
+        """
         return self.items == []
---
+++ estructuras_aux/test_Cola.py
@@ -0,0 +1,72 @@
+import unittest
+from Cola import Cola
+
+
+class TestCola(unittest.TestCase):
+    def setUp(self):
+        """
+            Instancia la clase Cola.
+        """
+        print('\nsetUp')
+        self.c = Cola()
+
+    def test_encolar(self):
+        """
+            Agrega un nuevo elemento al frente.
+        """
+        print('test_encolar')
+
+        c = self.c
+        c.encolar(1)
+        self.assertEqual(c.items, [1])
+        c.encolar(2)
+        self.assertEqual(c.items, [2, 1])
+        c.encolar(3)
+        self.assertEqual(c.items, [3, 2, 1])
+
+    def test_desencolar(self):
+        """
+            Desencola el primer elemento.
+        """
+        print('test_desencolar')
+
+        c = self.c
+        c.encolar(1)
+        c.encolar(2)
+        c.encolar(3)
+        self.assertEqual(c.desencolar(), 1)
+        self.assertEqual(c.desencolar(), 2)
+        self.assertEqual(c.desencolar(), 3)
+        with self.assertRaises(Exception) as context:
+            c.desencolar()
+        self.assertTrue('Cola vacía.' in str(context.exception))
+
+    def test_esta_vacia(self):
+        """
+            Chequea si la cola esta o no vacía.
+        """
+        print('test_esta_vacia')
+        c = self.c
+        self.assertEqual(c.esta_vacia(), True)
+        c.encolar(1)
+        self.assertEqual(c.esta_vacia(), False)
+        c.desencolar()
+        self.assertEqual(c.esta_vacia(), True)
+
+    def test_ver_frente(self):
+        """
+            Retorna el primer elemento manteniendo la cola intacta.
+        """
+        print('test_ver_frente')
+
+        c = self.c
+        self.assertEqual(c.ver_frente(), None)
+        c.encolar(1)
+        c.encolar(2)
+        self.assertEqual(c.ver_frente(), 1)
+        c.desencolar()
+        self.assertEqual(c.ver_frente(), 2)
+
+
+if __name__ == '__main__':
+    unittest.main()
