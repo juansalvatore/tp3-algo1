c361fbdac0e4c5d04a173589da55e94caf8547ed
Nahuel Hernan Spiguelman <nahuel.spiguelman1@hotmail.com>
2019-05-26 15:06:37 -0300

    agrega manejo de archivos y procesamiento de reglas, para generar el camino

--- estructuras/SistemaL.py
+++ estructuras/SistemaL.py
@@ -1,3 +1,7 @@
+from estructuras_aux.Cola import Cola
+from estructuras_aux.Pila import Pila
+from Tortuga import Tortuga
+
 class SistemaL:
     ''' Representación del SistemaL'''
 
@@ -16,20 +20,50 @@ class SistemaL:
             '[': 'apilar',
             ']': 'desapilar'
         }
-        self.pila_tortugas = Pila()
-        self.camino = Cola()
-
-    def apilar(self):
-        pass
-
-    def desapilar(self):
-        pass
+        
+        self.procesar_archivo(arch)
+        self.dibujar()
 
     def procesar_archivo(self, arch):
-        pass
-
-    def generar_camino(self):
-        pass
-
+        ''' Recibe un archivo, itera las primeras dos filas del mismo. Y llama a otro método que normalice
+            las reglas '''
+        try:
+            with open(arch, 'r') as archivo:
+                self.angulo = float(next(archivo).rstrip('\n'))
+                self.axioma = next(archivo).rstrip('\n')
+                self._normalizar_reglas(archivo)
+        except IOError as err:
+            return err
+    
     def dibujar(self):
+        ''' Empieza el dibujo de la tortuga '''
+        tortuga = Tortuga()
+        
+
+    def _normalizar_reglas(self, archivo):
+        ''' Recibe un archivo ya iterado dos posiciones, que solo tiene que iterar sobre las reglas.
+            Usa el atributo self.reglas como diccionario para guardar { 'caracter': 'regla' } '''
+        for regla in archivo:
+            caracter, regla = regla.rstrip('\n').split(' ')
+            self.reglas[caracter] = regla
+    
+    def _generar_camino(self, resultado, iteracion = 20):
+        ''' Método recursivo para la generación del camino que recorrerán las tortugas utilizadas '''
+        if iteracion > 0:
+            nueva_cadena = ''
+            for letra in resultado:
+                if letra in self.reglas:
+                    nueva_cadena += self.reglas[letra]
+                else:
+                    nueva_cadena += letra
+            resultado = nueva_cadena
+            return self._generar_camino(resultado, iteracion-1)
+        return resultado
+
+    def _apilar(self):
         pass
+
+    def _desapilar(self):
+        pass
+
+        
